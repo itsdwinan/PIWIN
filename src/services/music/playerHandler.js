60933e473bf34a7f31bf7810ba1039f7daf91f74
itsdwinan <itsdwinan@gmail.com>
2026-09-20 22:59:02 +0700

    Update playerHandler.js

--- src/services/music/playerHandler.js
+++ src/services/music/playerHandler.js
@@ -1,6 +1,5 @@
 // Player event handlers for Riffy. Adapted from Musicify playerHandler (Apache-2.0).
 
-import { getGuildConfig, updateGuildConfig } from '../../services/config/guildConfig.js';
 import { logger } from '../../utils/logger.js';
 import { getGuildMusicData, clearUpdateInterval } from './playerStore.js';
 import {
